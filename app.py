f0ab8293cde513af6a4df4608e841888353f8f7f
Travis Genovese <travis.genovese@gmail.com>
2018-09-24 20:28:21 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, redirect
+import pymongo
+import scrape_mars
+import pandas as pd
+
+app = Flask(__name__)
+
+# Create connection to MongoDB database `weather_db` and collection `forecasts`
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client.mars_db
+collection = db.mission
+
+# Create route that renders index.html template and finds documents from mongo
+@app.route('/')
+def index():
+    mission = collection.find()
+    # if 'Table' in mission:
+    #     table_dict = mission[0]['Table']
+    #     df = pd.DataFrame.from_dict(table_dict)
+    #     table_html = df.to_html
+    # else:
+    #     table_html = None
+    return render_template('index.html', mission=mission)
+
+# Create route that will trigger scrape functions
+@app.route('/scrape')
+def scrape():
+    mars_data = scrape_mars.scrape()
+
+    # Combine results into one dictionary
+    # forecast = {
+    #     'location': weather['location'],
+    #     'date': weather['date'],
+    #     'max_temp': weather['max_temp'],
+    #     'min_temp': weather['min_temp'],
+    #     'surf_spot': surf['surf_spot'],
+    #     'surf_height': surf['surf_height'],
+    # }
+
+    # Insert forecast into database
+    collection.insert_one(mars_data)
+
+    # Redirect back to home page
+    return redirect('/', code=302)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ scrape_mars.py
@@ -0,0 +1,91 @@
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+import pandas as pd
+
+def scrape():
+
+    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    mars_url = 'https://mars.nasa.gov/news/'
+    jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars' 
+    twitter_url = 'https://twitter.com/marswxreport?lang=en'
+    hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    #def scrape():
+
+    ##################################################################################
+    # NASA Masrs News Section
+    ##################################################################################
+
+    browser.visit(mars_url)
+    mars_html = browser.html
+    mars_soup = bs(mars_html, 'html.parser')
+
+    news_title = mars_soup.find('div', class_='content_title').text
+    news_p = mars_soup.find('div', class_='article_teaser_body').text
+
+    ##################################################################################
+    # Mars Featured Image Section
+    ##################################################################################
+
+    browser.visit(jpl_url)
+
+    jpl_html = browser.html
+    jpl_soup = bs(jpl_html, 'html.parser')
+
+    article = jpl_soup.find('article')
+    featured_image_url = ('https://www.jpl.nasa.gov' + article['style'][23:-3])
+
+    ##################################################################################
+    # Mars Weather Section
+    ##################################################################################
+
+    browser.visit(twitter_url)
+
+    twitter_html =  browser.html
+    twitter_soup =  bs(twitter_html, 'html.parser')
+
+    mars_weather = twitter_soup.find('p', class_='TweetTextSize TweetTextSize--normal js-tweet-text tweet-text').text
+
+    ##################################################################################
+    # Mars Facts Table
+    ##################################################################################
+
+    mars_facts = pd.read_html('https://space-facts.com/mars/')
+
+    mars_facts_dict = mars_facts[0].to_dict(orient='list')
+
+    ##################################################################################
+    # Hemispheres Section
+    ##################################################################################
+
+    browser.visit(hemi_url)
+    results = browser.find_by_css('div.description a.product-item')
+
+    links = []
+
+    for result in results:
+            links.append({'title': result.text, 'img_dict': result['href']})
+
+
+    hemisphere_image_urls = []
+
+    for link in links:
+            browser.visit(link['img_dict'])
+            img_url = browser.find_by_css('img.wide-image')['src']
+            hemisphere_image_urls.append({
+                    'title': link['title'].replace(' Enhanced', ''),
+                    'img_url': img_url,
+            })
+
+    scraped_data = {
+    'News Title': news_title,
+    'News Paragraph': news_p,
+    'Featured Image': featured_image_url,
+    'Mars Weather': mars_weather,
+    'Hemispheres': hemisphere_image_urls,
+    'Table': mars_facts_dict
+    }
+
+    return scraped_data
